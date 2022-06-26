450a18c45c84dd427da6e755384425859d3a8abc
kaleb <mengejemb@gmail.com>
2022-06-26 12:09:09 -0400

    adding another method

--- SeleniumTestingMVN/src/test/java/testcases/BrowserTest.java
+++ SeleniumTestingMVN/src/test/java/testcases/BrowserTest.java
@@ -19,7 +19,19 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class BrowserTest {
 	
 	
-	
+	public static void main(String[] args) {
+
+    System.setProperty("webdriver.chrome.driver", "X://chromedriver.exe");
+    WebDriver driver = new ChromeDriver();
+    driver.get("http://demo.guru99.com/test/ajax.html");
+    List<WebElement> elements = driver.findElements(By.name("name"));
+    System.out.println("Number of elements:" +elements.size());
+
+    for (int i=0; i<elements.size();i++){
+      System.out.println("Radio button text:" + elements.get(i).getAttribute("value"));
+    }
+  }
+}
 
 	
 	
